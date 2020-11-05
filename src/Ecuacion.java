69a4f007e2b5f88b8b2b1b434dcee0d9405d46f9
carlosarguelles <carlxsnap@192.168.2.1>
2020-11-05 01:57:02 -0500

    beta6

--- src/Ecuacion.java
+++ src/Ecuacion.java
@@ -1,3 +1,4 @@
+
 import java.text.DecimalFormat;
 
 public class Ecuacion {
@@ -68,7 +69,7 @@ public class Ecuacion {
     public double getC2() {
         return c2;
     }
-    
+
     public char getCaso() {
         return this.caso;
     }
@@ -162,7 +163,7 @@ public class Ecuacion {
                 break;
         }
     }
-    
+
     public boolean isEntero(double numero) {
         String num = String.valueOf(numero);
         float decNum = Float.parseFloat(num.substring(num.indexOf('.')));
@@ -174,7 +175,7 @@ public class Ecuacion {
             return false;
         }
     }
-    
+
     public String toEntero(double numero) {
         DecimalFormat df = new DecimalFormat("#0.00");
         String num = String.valueOf(numero);
@@ -188,7 +189,7 @@ public class Ecuacion {
             return doubleToFraccion(numero);
         }
     }
-    
+
     public double dividir(double a, double b) {
         return a / b;
     }
@@ -206,185 +207,200 @@ public class Ecuacion {
                 {
                     raicesS[0] = toEntero(dividir(A.getNumerador(), A.getDenominador()) + dividir(B.getNumerador(), B.getDenominador()));
                     raicesS[1] = toEntero(dividir(A.getNumerador(), A.getDenominador()) - dividir(B.getNumerador(), B.getDenominador()));
-                } 
-                else if (A.isFraccionEntera() && !B.isFraccionEntera())
+                } else if (A.isFraccionEntera() && !B.isFraccionEntera())
                 {
                     raicesS[0] = toEntero(dividir(A.getNumerador(), A.getDenominador())) + "+" + B.toString();
                     raicesS[1] = toEntero(dividir(A.getNumerador(), A.getDenominador())) + "-" + B.toString();
-                } 
-                else if (!A.isFraccionEntera() && B.isFraccionEntera())
+                } else if (!A.isFraccionEntera() && B.isFraccionEntera())
                 {
                     raicesS[0] = A.toString() + "+" + toEntero(dividir(B.getNumerador(), B.getDenominador()));
                     raicesS[1] = A.toString() + "-" + toEntero(dividir(B.getNumerador(), B.getDenominador()));
-                } 
-                else
+                } else
                 {
                     Fraccion a = new Fraccion((A.getNumerador() + B.getNumerador()), A.getDenominador());
                     Fraccion b = new Fraccion((A.getNumerador() - B.getNumerador()), A.getDenominador());
                     raicesS[0] = a.toString();
                     raicesS[1] = b.toString();
                 }
-            } 
-            else
+            } else
             {
                 raicesS[0] = "\\left(" + A.toString() + "+" + B.toString() + "\\right)";
                 raicesS[1] = "\\left(" + A.toString() + "-" + B.toString() + "\\right)";
             }
             r = "y(x)=c_{1}e^{" + raicesS[0] + "x}+c_{2}e^{" + raicesS[1] + "x}";
-            if(raicesS[0].equals(0)) {
+            if (raicesS[0].equals(0))
+            {
                 r = "y(x)=c_{1}+c_{2}e^{" + raicesS[1] + "x}";
             }
-            if(raicesS[1].equals(0)) {
+            if (raicesS[1].equals(0))
+            {
                 r = "y(x)=c_{1}e^{" + raicesS[0] + "x}+c_{2}";
             }
-            if(raicesS[0].equals(1)) {
+            if (raicesS[0].equals(1))
+            {
                 r = "y(x)=c_{1}e^{x}+c_{2}e^{" + raicesS[1] + "x}";
             }
-            if(raicesS[1].equals(1)) {
+            if (raicesS[1].equals(1))
+            {
                 r = "y(x)=c_{1}e^{" + raicesS[0] + "x}+c_{2}e^{x}";
             }
             return r;
-        }
-        else if(this.caso == '2') 
+        } else if (this.caso == '2')
         {
             raicesS[0] = getRaices(0).toString();
             raicesS[1] = getRaices(1).toString();
             return "y(x)=c_{1}e^{(" + raicesS[0] + ")x}\\cos\\left(" + raicesS[1] + "x\\right)+c_{2}e^{(" + raicesS[0] + ")x}\\sin\\left(" + raicesS[1] + "x\\right)";
-        } 
-        else 
+        } else
         {
             raicesS[0] = getRaices(0).toString();
             return "y(x)=c_{1}e^{" + raicesS[0] + "x}+c_{2}xe^{" + raicesS[0] + "x}";
         }
     }
-    
+
     private String getSigno(double n) {
         String aux = "";
-        if(n > 0) {
+        if (n > 0)
+        {
             aux = "+" + toEntero(n);
-        } else if (n < 0) {
+        } else if (n < 0)
+        {
             aux = "-" + toEntero(n * -1);
-        } else {
+        } else
+        {
             aux = "";
         }
         return aux;
     }
-    
+
     private String doubleToFraccion(double num) {
         double numerador = num, denominador = 1;
-        do {
+        do
+        {
             denominador++;
-        }
-        while(!isEntero(numerador * denominador));
+        } while (!isEntero(numerador * denominador));
         return "\\frac{" + toEntero(numerador * denominador) + "}{" + toEntero(denominador) + "}";
     }
-    
+
     public String ecuacionCaracteristicaToString() {
         String A = getSigno(a), B = getSigno(b), C = getSigno(c);
-        if(A.startsWith("+")) {
-            if(a == 1) {
+        if (A.startsWith("+"))
+        {
+            if (a == 1)
+            {
                 A = "r^{2}";
-            } else {
-                 A = toEntero(a) + "r^{2}";
-            } 
-            if(A.equals("")) {
+            } else
+            {
+                A = toEntero(a) + "r^{2}";
+            }
+            if (A.equals(""))
+            {
                 A = "";
             }
         }
-        if(B.equals("")) {
+        if (B.equals(""))
+        {
             B = "";
-        } else {
-            if(b == 1 || b == -1) {
+        } else
+        {
+            if (b == 1 || b == -1)
+            {
                 B = B.charAt(0) + "r";
-            } else {
+            } else
+            {
                 B = B + "r";
             }
         }
-        if(C.equals("")) {
+        if (C.equals(""))
+        {
             C = "";
-        } else {
+        } else
+        {
             C = C + "";
         }
-        return A + B + C + "=0"; 
+        return A + B + C + "=0";
     }
-    
+
     public String raicesToString() {
         String r = "";
-        switch(caso) {
+        switch (caso)
+        {
             case '1':
-                r = "r=\\dfrac{-\\left(" + toEntero(b) + "\\right)\\pm\\sqrt{\\left(" 
-                + toEntero(b) + "^{2}\\right)-4(" + toEntero(a) + ")(" + toEntero(c) 
-                + ")}}{2(" + toEntero(a) + ")}\\quad r_{1}=" + raicesS[0] + ", r_{2}=" + raicesS[1];
+                r = "r=\\dfrac{-\\left(" + toEntero(b) + "\\right)\\pm\\sqrt{\\left("
+                        + toEntero(b) + "^{2}\\right)-4(" + toEntero(a) + ")(" + toEntero(c)
+                        + ")}}{2(" + toEntero(a) + ")}\\quad r_{1}=" + raicesS[0] + ", r_{2}=" + raicesS[1];
                 break;
             case '2':
-                r = "r=\\dfrac{-\\left(" + toEntero(b) + "\\right)\\pm\\sqrt{\\left(" 
-                + toEntero(b) + "^{2}\\right)-4(" + toEntero(a) + ")(" + toEntero(c) 
-                + ")}}{2(" + toEntero(a) + ")}\\quad r="+ getRaices(0).toString()+"\\pm"+getRaices(1).toString()+"i \\Rightarrow \\lambda=" + raicesS[0] + ",\\mu=" + raicesS[1];
+                r = "r=\\dfrac{-\\left(" + toEntero(b) + "\\right)\\pm\\sqrt{\\left("
+                        + toEntero(b) + "^{2}\\right)-4(" + toEntero(a) + ")(" + toEntero(c)
+                        + ")}}{2(" + toEntero(a) + ")}\\quad r=" + getRaices(0).toString() + "\\pm" + getRaices(1).toString() + "i \\Rightarrow \\lambda=" + raicesS[0] + ",\\mu=" + raicesS[1];
                 break;
             case '3':
-                r = "r=\\dfrac{-\\left(" + toEntero(b) + "\\right)\\pm{\\sqrt{\\left(" 
-                + toEntero(b) + "^{2}\\right)-4(" + toEntero(a) + ")(" + toEntero(c) 
-                + ")}}}{2(" + toEntero(a) + ")}\\quad r_{2}=r_{1}=" + raicesS[0];
+                r = "r=\\dfrac{-\\left(" + toEntero(b) + "\\right)\\pm{\\sqrt{\\left("
+                        + toEntero(b) + "^{2}\\right)-4(" + toEntero(a) + ")(" + toEntero(c)
+                        + ")}}}{2(" + toEntero(a) + ")}\\quad r_{2}=r_{1}=" + raicesS[0];
                 break;
         }
         return r;
     }
-    
+
     public double hallarDet(double a, double b, double c, double d) {
         return (a * b) - (c * d);
     }
-    
+
     public void calcularPVI() {
-        double detg = 0, detx = 0, dety = 0;
+        double detg = 0, detx = 0, dety = 0, A = 0, B = 0, E = 0, D = 0;
         sistema = new double[2][3];
-        if(this.caso == '1') {
+        if (this.caso == '1')
+        {
             double r1 = dividir(this.raicesReducidas[0].getNumerador(), this.raicesReducidas[0].getDenominador());
             double r2 = dividir(this.raicesReducidas[1].getNumerador(), this.raicesReducidas[1].getDenominador());
-            if(this.pv1[0] == 0) {
-                sistema[0][0] = 1;
-                sistema[0][1] = 1;
-                sistema[0][2] = this.pv1[1];
-            }
-            if(this.pv2[0] == 0) {
-                sistema[1][0] = r1;
-                sistema[1][1] = r2;
-                sistema[1][2] = this.pv2[1];
-            }
+            A = Math.pow(Math.E, r1 * this.pv1[0]);
+            B = Math.pow(Math.E, r2 * this.pv1[0]);
+            sistema[0][0] = A;
+            sistema[0][1] = B;
+            sistema[0][2] = this.pv1[1];
+            D = Math.pow(Math.E, r1 * this.pv1[0]);
+            E = Math.pow(Math.E, r2 * this.pv1[0]);
+            sistema[1][0] = r1 * D;
+            sistema[1][1] = r2 * E;
+            sistema[1][2] = this.pv2[1];
             detg = hallarDet(sistema[0][0], sistema[1][1], sistema[1][0], sistema[0][1]);
             detx = hallarDet(sistema[0][1], sistema[1][2], sistema[1][1], sistema[0][2]);
             dety = hallarDet(sistema[0][0], sistema[1][2], sistema[1][0], sistema[0][2]);
             this.c1 = detx / detg * - 1;
-            this.c2 = dety / detg;     
-        } else if(this.caso == '2') {
+            this.c2 = dety / detg;
+        } else if (this.caso == '2')
+        {
             double r1 = dividir(this.raicesReducidas[0].getNumerador(), this.raicesReducidas[0].getDenominador());
             double r2 = dividir(this.raicesReducidas[1].getNumerador(), this.raicesReducidas[1].getDenominador());
-            if(this.pv1[0] == 0) {
-                sistema[0][0] = 1;
-                sistema[0][1] = 0;
-                sistema[0][2] = this.pv1[1];
-            }
-            if(this.pv2[0] == 0) {
-                sistema[1][0] = r1;
-                sistema[1][1] = r2;
-                sistema[1][2] = this.pv2[1];
-            }
+            A = Math.pow(Math.E, r1 * this.pv1[0]) * Math.cos(r2 * this.pv1[0]);
+            B = Math.pow(Math.E, r1 * this.pv1[0]) * Math.sin(r2 * this.pv1[0]);
+            sistema[0][0] = A;
+            sistema[0][1] = B;
+            sistema[0][2] = this.pv1[1];
+            D = (r1 * Math.pow(Math.E, r1 * this.pv1[0]) * Math.cos(r2 * this.pv1[0])) - (r2 * Math.pow(Math.E, r1 * this.pv1[0]) * Math.sin(r2 * this.pv1[0]));
+            E = (r1 * Math.pow(Math.E, r1 * this.pv1[0]) * Math.sin(r2 * this.pv1[0])) + (r2 * Math.pow(Math.E, r1 * this.pv1[0]) * Math.cos(r2 * this.pv1[0]));
+            sistema[1][0] = D;
+            sistema[1][1] = E;
+            sistema[1][2] = this.pv2[1];
             detg = hallarDet(sistema[0][0], sistema[1][1], sistema[1][0], sistema[0][1]);
             detx = hallarDet(sistema[0][1], sistema[1][2], sistema[1][1], sistema[0][2]);
             dety = hallarDet(sistema[0][0], sistema[1][2], sistema[1][0], sistema[0][2]);
             this.c1 = detx / detg * -1;
             this.c2 = dety / detg;
-        } else {
+        } else
+        {
             double r1 = dividir(this.raicesReducidas[0].getNumerador(), this.raicesReducidas[0].getDenominador());
-            if(this.pv1[0] == 0) {
-                sistema[0][0] = r1;
-                sistema[0][1] = 0;
-                sistema[0][2] = this.pv1[1];
-            }
-            if(this.pv2[0] == 0) {
-                sistema[1][0] = r1;
-                sistema[1][1] = Math.E;
-                sistema[1][2] = this.pv2[1];
-            }
+            A = Math.pow(Math.E, r1 * this.pv1[0]);
+            B = this.pv1[0] * Math.pow(Math.E, r1 * this.pv1[0]);
+            sistema[0][0] = A;
+            sistema[0][1] = B;
+            sistema[0][2] = this.pv1[1];
+            D = r1 * Math.pow(Math.E, r1 * this.pv1[0]);
+            E = (r1 * Math.pow(Math.E, this.pv1[0])) + (Math.pow(Math.E, r1 * this.pv1[0]));
+            sistema[1][0] = D;
+            sistema[1][1] = E;
+            sistema[1][2] = this.pv2[1];
             detg = hallarDet(sistema[0][0], sistema[1][1], sistema[1][0], sistema[0][1]);
             detx = hallarDet(sistema[0][1], sistema[1][2], sistema[1][1], sistema[0][2]);
             dety = hallarDet(sistema[0][0], sistema[1][2], sistema[1][0], sistema[0][2]);
